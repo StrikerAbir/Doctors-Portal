104814aa1e372d41060f55508af821c39193dff9
Abir Hasan <cse.170201063@gmail.com>
2022-11-16 20:59:13 +0600

    server setup

---
+++ doctors-portal-server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 1000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send('doctors portal running');
+})
+
+app.listen(port,()=>{
+    console.log('doctors portal running on',port)
+})
