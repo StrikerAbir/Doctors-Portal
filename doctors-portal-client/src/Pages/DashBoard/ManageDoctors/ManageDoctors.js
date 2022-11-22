7de53d39d055069becf735b2aefec64fda719454
Abir Hasan <cse.170201063@gmail.com>
2022-11-22 19:10:41 +0600

    Delete User VerifyAdmin middleware and load all doctors

--- doctors-portal-client/src/Pages/DashBoard/ManageDoctors/ManageDoctors.js
+++ doctors-portal-client/src/Pages/DashBoard/ManageDoctors/ManageDoctors.js
@@ -1,10 +1,72 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from '../../../Shared/Loading';
 
 const ManageDoctors = () => {
+
+    const { data: doctors,isLoading } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
+            try {
+                const res = await fetch("http://localhost:1000/doctors", {
+                    headers: {
+                        authorization:`bearer ${localStorage.getItem('accessToken')}`
+                    }
+                })
+                const data = await res.json();
+                return data;
+            }
+            catch (e) {
+
+            }
+        }
+    })
+    if (isLoading) {
+        <Loading></Loading>
+    }
+
     return (
+      <div className="bg-slate-100 p-10">
+        <h3 className="text-3xl my-5 bg">Manage Doctors {doctors?.length}</h3>
         <div>
-            manageDoctors
+          <div className="overflow-x-auto">
+            <table className="table w-full">
+              {/* <!-- head --> */}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Avatar</th>
+                  <th>Name</th>
+                  <th>Specialty</th>
+                  <th>E-mail</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* <!-- row 1 --> */}
+                {doctors?.map((doctor, index) => (
+                  <tr key={doctor._id}>
+                    <th>{index + 1}</th>
+                    <td>
+                      <div className="avatar">
+                        <div className="w-24 rounded-full">
+                          <img alt='' src={doctor.image} />
+                        </div>
+                      </div>
+                    </td>
+                    <td>{doctor.name}</td>
+                    <td>{doctor.specialty}</td>
+                    <td>{doctor.email}</td>
+                    <td>
+                      <button className="btn btn-xs bg-red-700">Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
+      </div>
     );
 };
 
